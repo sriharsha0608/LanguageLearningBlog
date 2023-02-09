6f7c3c9ce591adcd94cee666e6f241af7657e1e8
sriharsha0608 <120688467+sriharsha0608@users.noreply.github.com>
2023-02-08 21:38:35 -0600

    Create variables.js

---
+++ code/variables.js
@@ -0,0 +1,17 @@
+//Using var
+var favoriteFood='pizza';
+var numOfSlices=8;
+console.log(favoriteFood);
+console.log(numOfSlices);
+
+//Using let
+let changeMe=true;
+changeMe=false;
+console.log(changeMe);
+
+//Using Const 
+const entree='Enchiladas';
+console.log(entree);
+entree = 'Tacos' // This will result in TypeError.
+
+

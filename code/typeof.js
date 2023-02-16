989aa18695ee205cf3d0f1204c19992193d87380
sriharsha0608 <120688467+sriharsha0608@users.noreply.github.com>
2023-02-15 20:55:42 -0600

    Create typeof.js

---
+++ code/typeof.js
@@ -0,0 +1,11 @@
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable); //Output :string
+newVariable = 1;
+console.log(typeof newVariable); //Output: number
+console.log(typeof newVariable); //Output: number
+
+let name='Harsha';
+let age = 23;
+sentence= name + age;
+console.log(sentence); // Prints Harsha23
+console.log(typeof sentence); //Output: string

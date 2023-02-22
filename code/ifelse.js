4a055c59667a3cbd55f67c79ff5bf92b79767521
sriharsha0608 <120688467+sriharsha0608@users.noreply.github.com>
2023-02-22 17:27:58 -0600

    Create ifelse.js

---
+++ code/ifelse.js
@@ -0,0 +1,10 @@
+let sale = true;
+
+sale = false; // reassigning sale
+
+if(sale) {
+  console.log('Time to buy!');
+}
+else{
+  console.log('Time to wait for a sale.');
+} // Prints Time to wait for a sale.

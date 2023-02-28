a4c461489e310895ca4be613fd6fa46c040de7e8
sriharsha0608 <120688467+sriharsha0608@users.noreply.github.com>
2023-02-28 14:56:28 -0600

    Create ternary.js

---
+++ code/ternary.js
@@ -0,0 +1,11 @@
+let isLocked=true;
+isLocked? console.log('You will need a key to open the door.'): console.log('You will not need a key to open the door.')
+// Prints You will need a key to open the door.
+
+let isCorrect=0;
+isCorrect? console.log('Correct!'):console.log('Incorrect!');
+// Prints Incorrect!
+
+let favoritePhrase=''
+favoritePhrase==='Love That!'? console.log('I love that!'):console.log('I don\'t love that!');
+// Prints I don't love that!

5da1feabb61086cb886a04b3394d9d666d574917
sriharsha0608 <120688467+sriharsha0608@users.noreply.github.com>
2023-03-10 17:52:06 -0600

    Create callingfunction.js

---
+++ code/callingfunction.js
@@ -0,0 +1,8 @@
+function sayThanks(){
+  console.log('Thank you for your purchase! We appreciate your business.');
+}
+sayThanks();
+// Calling it three times
+sayThanks();
+sayThanks();
+sayThanks();

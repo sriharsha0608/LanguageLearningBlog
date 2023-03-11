877cc6bc3bac2dabaa0ff4aaf72cb55b95bff572
sriharsha0608 <120688467+sriharsha0608@users.noreply.github.com>
2023-03-10 18:02:52 -0600

    Create functionparam.js

---
+++ code/functionparam.js
@@ -0,0 +1,13 @@
+function sayThanks(name) {
+  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+}
+
+sayThanks('Cole');
+
+// Default Parameters
+function makeShoppingList(item1='milk', item2='bread', item3='eggs'){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+makeShoppingList();
